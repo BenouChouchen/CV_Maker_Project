d1d40d8a9ae34d7ae6ccbcf8d390a6dff777692c
BenouChouchen <benjamin.droulon@gmail.com>
2018-07-12 21:28:13 +0200

    Add files via upload
    Uploading new files

--- src/js/main.js
+++ src/js/main.js
@@ -15,51 +15,9 @@ $(document).ready(function() {
 			}
 		)
 	})
-
 var dropdown = document.querySelector('.dropdown');
 dropdown.addEventListener('click', function(event) {
   event.stopPropagation();
   dropdown.classList.toggle('is-active');
 });
-
-	//send mail with ajax
-/*
-	$('#send_email').click(function(e){
-		e.preventDefault();
-		//classe date
-		var data = {
-			email: $('#email').val(),
-			name: $('#name').val(),
-			objet: $('#firstname').val(),
-			message: $('#message').val()
-		};
-		$.ajax({
-			url: "mail.php",
-			type: 'POST',
-			data: data,
-			//Callbacks (succès ou erreur)
-			success: function(data) {
-				$('#js_alert_success').css({'display' : 'block'});
-				setTimeout(function(){ // On attend 3 secondes
-					$('#js_alert_success').css({'display' : 'none'});
-					//vider les champs
-					$('#email').val("");
-					$('#name').val("");
-					$('#firstname').val("");
-					$('#message').val("")
-				}, 3000);
-			},
-			error: function(data) {
-				$('#js_alert_danger').css({'display' : 'block'});
-				setTimeout(function(){
-					$('#js_alert_danger').css({'display' : 'none'});
-					$('#email').val("");
-					$('#name').val("");
-					$('#firstname').val("");
-					$('#message').val("")
-				}, 3000);
-			}
-		});
-	});
-*/
 });
